91b28797549b7db24a84b25d20a1bcc2365c469b
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-07-07 00:04:02 +0800

    feature: Add search functionality

--- js/search.js
+++ js/search.js
@@ -5,10 +5,21 @@ Most browsers no longer do that: they wait until the loop finishes and then they
 
 Sorry for the confusion, and we'll update the video soon. */
 
-var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
+var inStock = ['apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
 var search;
 
-function print(message) {
-  document.write( '<p>' + message + '</p>');
+while (true) {
+  search = prompt('Search for a product in our store. Type "list" to show all of the produce and "quit" to exit.');
+  search = search.toLowerCase();
+  if (search === 'quit') {
+    break;
+  } else if (search === 'list') {
+    document.write('<p>' + inStock.join(', ') + '</p>');
+  } else {
+    if (inStock.indexOf(search) !== -1) {
+      document.write('Yes, we have ' + search + ' in the store.');
+    } else {
+      document.write(search + ' is not in stock.');
+    }
+  }
 }
-
